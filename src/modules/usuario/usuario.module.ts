abba2707d218528e38bd426efe7615a86876ce2d
ShootDomy <canizaresdomenica4@gmail.com>
2025-08-18 12:40:18 -0500

    feat(usuario): implement user registration and login functionality
    - Added JWT authentication with registration and login endpoints.
    - Removed unused DTOs for user creation and update.
    - Introduced new DTOs for user registration and login with validation.
    - Updated UsuarioService to handle user registration and login logic.
    - Integrated bcrypt for password hashing and validation.
    - Configured TypeORM and JWT modules in UsuarioModule.
    - Enhanced error handling for user-related operations.

--- src/modules/usuario/usuario.module.ts
+++ src/modules/usuario/usuario.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './usuario.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [UsuarioController],
-  providers: [UsuarioService],
+  providers: [UsuarioService, ConfigService],
+  imports: [
+    TypeOrmModule.forFeature([Usuario]),
+    JwtModule.registerAsync({
+      imports: [],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
+    }),
+  ],
+  exports: [UsuarioService],
 })
 export class UsuarioModule {}
